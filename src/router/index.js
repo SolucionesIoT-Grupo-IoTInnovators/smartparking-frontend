99e0f712c01718921c6aceb31c6e827322b08413
JP19-03 <u202014511@upc.edu.pe>
2025-05-03 20:18:45 -0500

    chore: configure router instance.

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,56 @@
 import {createRouter, createWebHistory} from "vue-router";
+import ParkingDirectoryComponent from "../parking-management/pages/parking-directory.component.vue";
 import ParkingRegistrationComponent from "../parking-management/pages/parking-registration.component.vue";
 import ParkingHomeComponent from "../parking-management/pages/parking-home.component.vue";
+import SignInComponent from "../iam/pages/sign-in.component.vue";
+import {authenticationGuard} from "../iam/services/authentication.guard.js";
 
-const router= createRouter({
+const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/home', name: 'home', component: ParkingHomeComponent, meta: { title: 'Home'}},
-        { path: '/parking/registration', name: 'parking-registration', component: ParkingRegistrationComponent, meta: { title: 'Parking Registration'}},
-        { path: '/', redirect: '/home' },
+        {
+            path: '/parking/directory/:ownerId',
+            name: 'parking-directory',
+            component: ParkingDirectoryComponent,
+            meta: {title: 'Parking Directory', layout: 'ParkingManagementLayout'}
+        },
+        {
+            path: '/home/:parkingId',
+            name: 'home',
+            component: ParkingHomeComponent,
+            meta: {title: 'Home', layout: 'ParkingManagementLayout'}
+        },
+        {
+            path: '/parking/registration',
+            name: 'parking-registration',
+            component: ParkingRegistrationComponent,
+            meta: {title: 'Parking Registration'}
+        },
+        {
+            path: '/sign-in',
+            name: 'sign-in',
+            component: SignInComponent,
+            meta: {title: 'Sign In', layout: 'EmptyLayout'}
+        },
+        {
+            path: '/',
+            redirect: () => {
+                const userId = localStorage.getItem('userId');
+                if (!userId) {
+                    return { name: 'sign-in' };
+                }
+                return { name: 'parking-directory', params: { ownerId: userId } };
+            }
+        }
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    // Set the page title
+    let baseTitle = 'SmartParking System';
+    document.title = `${baseTitle} | ${to.meta['title']}`;
+    // Call the authentication guard
+    authenticationGuard(to, from, next);
+})
 
 export default router;
