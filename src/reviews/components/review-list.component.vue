d4992a654abc221d303d82c75164995ec2afc1ad
JP19-03 <u202014511@upc.edu.pe>
2025-05-07 21:48:37 -0500

    feat(reviews): add review card list component.
    Co-Authored-By: CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
    Co-Authored-By: FernandoDiazNeo <205968129+FernandoDiazNeo@users.noreply.github.com>
    Co-Authored-By: Josty Tafur <89022172+JostyTafur@users.noreply.github.com>
    Co-Authored-By: U202114545 <102629180+U202114545@users.noreply.github.com>

---
+++ src/reviews/components/review-list.component.vue
@@ -0,0 +1,26 @@
+<script>
+import ReviewCard from "./review-card.component.vue";
+
+export default {
+  name: "review-list",
+  components: {ReviewCard},
+  props: {
+    reviews: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="grid gap-4">
+    <div v-for="review in reviews" :key="review.id" class="p-4 border-round shadow-2 bg-white">
+      <review-card :review="review" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
