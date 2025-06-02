dab5471763da03186f708f407d3937d78cfa2f4e
ern23zc <U202112907@upc.edu.pe>
2025-06-02 11:26:08 -0500

    style: add Responsivity to Header

--- src/parking-management/components/parking-list.component.vue
+++ src/parking-management/components/parking-list.component.vue
@@ -22,7 +22,7 @@ export default {
 
 <template>
   <div class="p-5">
-    <div class="flex justify-content-between align-items-center mb-4">
+    <div class="flex justify-content-between align-items-center mb-4" id="header">
       <h1 class="text-4xl font-bold m-0 text-center">{{ $t('parkingList') }}</h1>
       <pv-button label="Add Parking" icon="pi pi-plus" @click="goToRegistration()"/>
     </div>
@@ -38,5 +38,10 @@ export default {
 </template>
 
 <style scoped>
-
+@media (max-width: 768px) {
+  #header {
+    flex-direction: column;
+    gap: 10px;
+  }
+}
 </style>
